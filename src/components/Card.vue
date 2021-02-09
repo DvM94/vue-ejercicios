fdba1e039d212cf3e142beb27a95cf9f076de398
DvM94 <danielvillarmartin@gmail.com>
2021-02-09 10:32:13 +0100

    Tarjetas creadas

---
+++ src/components/Card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <section class="image">
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </section>
+    <div class="text">
+      <h1>Esto es un título</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+        accusantium molestias quo consequuntur corporis illo libero, ex
+        architecto dolore ut facere repellendus assumenda vero nostrum sint
+        corrupti fugit est qui.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.card {
+  margin: 20px auto;
+  width: 500px;
+  display: flex;
+  justify-content: center;
+  border: 2px solid black;
+  border-radius: 6px;
+  box-shadow: 4px 4px 5px #888;
+  .image {
+    padding: 0 20px;
+    width: 200px;
+    display: flex;
+    align-items: center;
+  }
+  .text {
+    padding: 0 20px;
+    width: 200px;
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <Card/>
+  <Card/>
+  <Card/>
 </template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Card
+  }
+}
+</script>
